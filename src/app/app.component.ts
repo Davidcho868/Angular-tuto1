ffdca9c2de58aec2b7974545faf79ca01f51a5c2
David Chosson <davidcho868@gmail.com>
2023-07-27 11:53:37 +0200

    tuto 1 tour of heroes Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tour_of_heroes';
+  title = 'Tour of Heroes';
 }
